213facbd805c4acb354f862725a9dd85eae72d9d
Lucifer5642 <kharadiashu13@gmail.com>
2022-04-30 23:50:28 +0530

    First commit

---
+++ app/src/main/java/com/example/de/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.de;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Handler h = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Homescreen.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, 1000);
+    }
+}
